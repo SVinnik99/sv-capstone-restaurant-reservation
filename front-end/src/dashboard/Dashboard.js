c0caebe90a00ca78016f0c150abcdf228486dadf
Samuel Vinnik <themansamuel3@gmail.com>
2023-12-22 15:43:38 -0600

    data displays on dash board and defaults to today, next and previous days also work

--- front-end/src/dashboard/Dashboard.js
+++ front-end/src/dashboard/Dashboard.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { listReservations } from "../utils/api";
+import { today,previous,next } from "../utils/date-time";
 import ErrorAlert from "../layout/ErrorAlert";
+import "./Dashboard.css"
 
 /**
  * Defines the dashboard page.
@@ -11,6 +13,7 @@ import ErrorAlert from "../layout/ErrorAlert";
 function Dashboard({ date }) {
   const [reservations, setReservations] = useState([]);
   const [reservationsError, setReservationsError] = useState(null);
+  const [currentDate, setCurrentDate] = useState(today())
 
   useEffect(loadDashboard, [date]);
 
@@ -23,11 +26,29 @@ function Dashboard({ date }) {
     return () => abortController.abort();
   }
 
+  // Increments the day by 1 forwards
+  function handleNext(){
+    setCurrentDate(next(currentDate))
+  }
+// Increments the day by 1 backwards
+function handlePrevious(){
+  setCurrentDate(previous(currentDate))
+}
+// Brings you back to todays date
+function handleToday(){
+  setCurrentDate(today())
+}
+ 
   return (
     <main>
       <h1>Dashboard</h1>
       <div className="d-md-flex mb-3">
-        <h4 className="mb-0">Reservations for date</h4>
+        <h4 className="mb-0">Reservations for date: {currentDate} </h4>
+      </div>
+      <div>
+      <button  onClick={()=>handlePrevious()}type="button" class="btn btn-secondary btn-sm">Previous</button>
+      <button onClick={()=>handleToday()}type="button" class="btn btn-primary btn-sm">Today</button>
+      <button onClick={()=>handleNext()}type="button" class="btn btn-secondary btn-sm">Next</button>
       </div>
       <ErrorAlert error={reservationsError} />
       {JSON.stringify(reservations)}
