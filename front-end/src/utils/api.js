0b7d8db926903c2ecaa7ee995935f38942e74ad1
Samuel Vinnik <themansamuel3@gmail.com>
2024-01-11 16:40:11 -0600

    deployment?

--- front-end/src/utils/api.js
+++ front-end/src/utils/api.js
@@ -6,7 +6,7 @@ import formatReservationDate from "./format-reservation-date";
 import formatReservationTime from "./format-reservation-date";
 
 const API_BASE_URL =
-  process.env.REACT_APP_API_BASE_URL || "https://restaurant-reservation-backend-k5h3.onrender.com/";
+  process.env.REACT_APP_API_BASE_URL || "https://restaurant-reservation-backend-k5h3.onrender.com";
 
 /**
  * Defines the default headers for these functions to work with `json-server`
